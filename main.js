085c7a9d02af6c885c7537e9c145869fde1d67f5
DanielWarkentin123 <162719004+DanielWarkentin123@users.noreply.github.com>
2024-03-12 11:24:37 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMljQxgib4p8R7sa5VhQ04K0wjnkvpiJKUXQ&usqp=CAU"
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://static.quizur.com/i/b/585712ed5abdb1.84691938download.jpg";
+}
+
